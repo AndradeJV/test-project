670562cd529e8f8667803e0ac0eebdbab4e50a34
João Andrade <joaovandrade30@gmail.com>
2026-01-29 16:28:23 -0300

    update in response api

--- book-manager/frontend/src/App.jsx
+++ book-manager/frontend/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(bookData)
       })
-      if (!response.ok) throw new Error('Erro ao criar livros - Revise seu payload')
+      if (!response.ok) throw new Error('Erro ao criar livros')
       await fetchBooks()
       setError(null)
     } catch (err) {
